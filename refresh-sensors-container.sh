f87949d09a533362b259abebfae310a8927782f3
geostag <50096315+geostag@users.noreply.github.com>
2026-08-04 08:14:45 +0200

    prepare refresh_containers to work without checkout

--- refresh-sensors-container.sh
+++ refresh-sensors-container.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# read logs
 docker compose -f docker-compose-prod.yml logs sensors
 
+# update containers
 docker compose -f docker-compose-prod.yml up -d --pull always --force-recreate sensors
 
-git pull
+# update this script
+FILE=`basename $0`
+FILE_T="$FILE".`date "+%Y%m%d%H%M%S`
+
+curl -k -o "$FILE_T" https://raw.githubusercontent.com/geostag/smarthome/refs/heads/main/"$FILE" \
+&& chmod u+rx "$FILE_T" \
+&& mv "$FILE_T" "$FILE
